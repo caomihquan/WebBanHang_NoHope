84cdfcdb21ee6901f9279fd5423b67a8b75840b2
Thắng nè <45776208+thang10112000@users.noreply.github.com>
2021-11-07 20:01:36 +0700

    BingdingDuLieu/LyThuongThang
    Binding dữ liệu ra trang chủ sử dụng View Bag

--- WebBanHang_NoHope/WebBanHang_NoHope/Controllers/HomeController.cs
+++ WebBanHang_NoHope/WebBanHang_NoHope/Controllers/HomeController.cs
@@ -12,6 +12,10 @@ namespace WebBanHang_NoHope.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            ViewBag.Slides = new SlideDao().ListAll();
+            var productDao = new ProductDao();
+            ViewBag.NewProducts = productDao.ListNewProduct(4);
+            ViewBag.ListFeatureProducts = productDao.ListFeatureProduct(4);
             return View();
         }
         [ChildActionOnly]
