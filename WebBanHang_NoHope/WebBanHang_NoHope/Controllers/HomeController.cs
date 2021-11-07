3bb4c31f1943fe058ab59b303f486bc4553ffbac
Thắng nè <45776208+thang10112000@users.noreply.github.com>
2021-11-07 10:14:54 +0700

    updateHome/LyThuongThang
    Đọc dữ liệu cho các thành phần dùng chung

--- WebBanHang_NoHope/WebBanHang_NoHope/Controllers/HomeController.cs
+++ WebBanHang_NoHope/WebBanHang_NoHope/Controllers/HomeController.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Model.Dao;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace WebBanHang_NoHope.Controllers
+namespace WebBanHang_NoHope.Controllers 
 {
     public class HomeController : Controller
     {
@@ -12,6 +13,26 @@ namespace WebBanHang_NoHope.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        [ChildActionOnly]
+        public ActionResult MainMenu()
+        {
+            var model = new MenuDao().ListByGroupId(1);
+            return PartialView(model);
+        }
+        [ChildActionOnly]
+        [OutputCache(Duration = 3600 * 24)]
+        public ActionResult TopMenu()
+        {
+            var model = new MenuDao().ListByGroupId(2);
+            return PartialView(model);
+        }
+        [ChildActionOnly]
+        [OutputCache(Duration = 3600 * 24)]
+        public ActionResult Footer()
+        {
+            var model = new FooterDao().GetFooter();
+            return PartialView(model);
         }
     }
 }
