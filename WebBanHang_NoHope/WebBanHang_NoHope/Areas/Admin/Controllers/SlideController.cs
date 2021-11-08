711a09362f58b025f31984340a862910e3333120
minhphucpro <71248917+minhphucpro@users.noreply.github.com>
2021-11-07 20:53:45 -0800

    phucslide
    aaaaaaaaa

---
+++ WebBanHang_NoHope/WebBanHang_NoHope/Areas/Admin/Controllers/SlideController.cs
@@ -0,0 +1,82 @@
+﻿using Model.Dao;
+using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBanHang_NoHope.Areas.Admin.Controllers
+{
+    public class SlideController : BaseController
+    {
+        // GET: Admin/Slide
+        public ActionResult Index(string searchString, int page = 1, int pageSize = 10)
+        {
+            var dao = new SlideDao();
+            var model = dao.ListAllPaging(searchString, page, pageSize);
+            ViewBag.SearchString = searchString;
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var slide = new SlideDao().ViewDetail(id);
+            return View(slide);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Slide slide)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new SlideDao();
+                long id = dao.Insert(slide);
+                if (id > 0)
+                {
+                    SetAlert("Thêm Thành Công ", "success");
+                    return RedirectToAction("Index", "Slide");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Thêm  Không thành công");
+                }
+            }
+            return View("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Slide slide)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new SlideDao();
+                var result = dao.Update(slide);
+                if (result)
+                {
+                    SetAlert("Sửa Thành Công ", "success");
+
+                    return RedirectToAction("Index", "Slide");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Update Không thành công");
+                }
+            }
+            return View("Index");
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            new SlideDao().Delete(id);
+            return RedirectToAction("Index", "Slide");
+        }
+    }
+}
