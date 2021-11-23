ecf35c49a6275a1b7d1d2e2c6c19eff01f31950a
caomihquan <71248793+caomihquan@users.noreply.github.com>
2021-11-22 21:39:45 -0800

    quan
    aaa

--- WebBanHang_NoHope/WebBanHang_NoHope/Common/Encryptor.cs
+++ WebBanHang_NoHope/WebBanHang_NoHope/Common/Encryptor.cs
@@ -13,17 +13,17 @@ namespace WebBanHang_NoHope.Common
         {
             MD5 md5 = new MD5CryptoServiceProvider();
 
-            //compute hash from the bytes of text  
+            //compute hash from the bytes of text
             md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(text));
 
-            //get hash result after compute it  
+            //get hash result after compute it
             byte[] result = md5.Hash;
 
             StringBuilder strBuilder = new StringBuilder();
             for (int i = 0; i < result.Length; i++)
             {
-                //change it into 2 hexadecimal digits  
-                //for each byte  
+                //change it into 2 hexadecimal digits
+                //for each byte
                 strBuilder.Append(result[i].ToString("x2"));
             }
 
---
+++ WebBanHang_NoHope/WebBanHang_NoHope/Controllers/ContactController.cs
@@ -0,0 +1,63 @@
+﻿using Model;
+using Model.Dao;
+using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBanHang_NoHope.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            var model = new ContactDao().GetActiveContact();
+            return View(model);
+        }
+
+        [HttpPost]
+        public JsonResult Send(string name, string mobile, string address, string email, string content)
+        {
+            var feedback = new Feedback();
+            feedback.Name = name;
+            feedback.Email = email;
+            feedback.CreatedDate = DateTime.Now;
+            feedback.Phone = mobile;
+            feedback.Content = content;
+            feedback.Address = address;
+
+
+            string a = System.IO.File.ReadAllText(Server.MapPath("/Assets/Client/Template/NewFeedback.html"));
+            a = a.Replace("{{CustomerName}}", name);
+            a = a.Replace("{{Phone}}", mobile);
+            a = a.Replace("{{Email}}", email);
+            a = a.Replace("{{Address}}", address);
+            a = a.Replace("{{Require}}", content);
+            var toEmail = ConfigurationManager.AppSettings["ToEmailAddress"].ToString();
+
+            new MainHelper().SendMail(email, "Feedback Mới", a);
+            new MainHelper().SendMail(toEmail, "Feedback Mới", a);
+
+            var id = new ContactDao().InsertFeedBack(feedback);
+            if (id > 0)
+            {
+                return Json(new
+                {
+                    status = true
+                });
+                //send mail
+            }
+
+            else
+                return Json(new
+                {
+                    status = false
+                });
+
+        }
+    }
+}
