9b1f7332e583b5dab4056841bae1d7928c83b40e
nguyenduchuy0369 <71873334+nguyenduchuy0369@users.noreply.github.com>
2021-11-24 11:28:51 -0800

    huy
    done

--- WebBanHang_NoHope/Model/Dao/MenuDao.cs
+++ WebBanHang_NoHope/Model/Dao/MenuDao.cs
@@ -1,4 +1,5 @@
 ﻿using Model.EF;
+using PagedList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +19,67 @@ namespace Model.Dao
         public List<Menu> ListByGroupId(int groupId)
         {
             return db.Menus.Where(x => x.TypeID == groupId && x.Status == true).OrderBy(x => x.DisplayOrder).ToList();
+        }
+
+        public long Insert(Menu entity)
+        {
+            db.Menus.Add(entity);
+
+            db.SaveChanges();
+            return entity.ID;
+        }
+
+        public bool Update(Menu entity)
+        {
+            try
+            {
+                var menu = db.Menus.Find(entity.ID);
+                menu.Text = entity.Text;
+                menu.Link = entity.Link;
+                menu.DisplayOrder = entity.DisplayOrder;
+                menu.Target = entity.Target;
+                menu.TypeID = entity.TypeID;
+                menu.Status = entity.Status;
+
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+
+
+        public Menu ViewDetail(int id)
+        {
+            return db.Menus.Find(id);
+        }
+        public IEnumerable<Menu> ListAllPaging(string searchString, int page, int pageSize)
+        {
+            IQueryable<Menu> model = db.Menus;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                model = model.Where(x => x.Text.Contains(searchString) || x.Link.Contains(searchString));
+            }
+            return model.OrderByDescending(x => x.ID).ToPagedList(page, pageSize);
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                var menu = db.Menus.Find(id);
+                db.Menus.Remove(menu);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
         }
     }
 }
---
+++ WebBanHang_NoHope/WebBanHang_NoHope/Areas/Admin/Controllers/FooterController.cs
@@ -0,0 +1,49 @@
+﻿using Model.Dao;
+using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBanHang_NoHope.Areas.Admin.Controllers
+{
+    public class FooterController : BaseController
+    {
+        // GET: Admin/Footer
+        public ActionResult Index()
+        {
+            var dao = new FooterDao();
+            var model = dao.ListAll();
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(string id)
+        {
+            var footer = new FooterDao().GetByID(id);
+            return View(footer);
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Edit(Footer footer)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new FooterDao();
+                var result = dao.Update(footer);
+                if (result)
+                {
+                    SetAlert("Sửa Thành Công ", "success");
+                    return RedirectToAction("Index", "Footer");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Update Không thành công");
+                }
+            }
+            return View("Index");
+        }
+    }
+}
